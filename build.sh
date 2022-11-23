06802655a33fc4904f5773a420f31b86bde2a5a1
paulroth3d <paulroth3d@gmail.com>
2022-11-23 13:33:24 -0600

    UPDATE: build.sh until automated build is working

--- build.sh
+++ build.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 unset ijsBuildVersion
-export ijsBuildVersion=1.22.0
+export ijsBuildVersion=1.22.3
+
+echo "have you already published the latest npm package?"
+
+read npmPublished
+
+if [ "${npmPublished}" != "y" ];
+then
+	return 0
+fi
+
+echo "have you updated the package?"
+
+read packageUpdated
+
+if [ "${packageUpdated}" != "y" ];
+then
+	return 0
+fi
 
 echo "will build new version:${ijsBuildVersion}"
 echo "should we proceed?"
